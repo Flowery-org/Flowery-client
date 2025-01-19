5a4d6d4e4b74c75328b7369cf705d8fababc31f8
hjin03 <cats55655@naver.com>
2025-01-19 16:14:02 +0900

    feat(client): modify button component

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { cn } from '@packages/ui/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-base font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
@@ -19,17 +19,26 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        green:
+          'bg-[#619F56] border-1 border-[#5A7854] text-white hover:bg-[#619F56]/90',
+
+        gray: 'bg-[#989693] text-white hover:bg-[#989693]/90',
+      },
+      shape: {
+        default: 'rounded-xl',
       },
       size: {
         default: 'h-9 px-4 py-2',
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-8',
         icon: 'h-9 w-9',
+        full: 'h-12 w-full',
       },
     },
     defaultVariants: {
       variant: 'default',
       size: 'default',
+      shape: 'default',
     },
   },
 );
