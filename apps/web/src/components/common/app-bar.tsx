9ae678f61a86a52413199ba8a2b60b5f0fe97f10
hjin03 <cats55655@naver.com>
2025-01-20 14:58:35 +0900

    feat(client): app-bar component

---
+++ apps/web/src/components/common/app-bar.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import { ReactNode } from 'react';
+
+type AppBarProps = {
+  title?: string;
+  titleStyle?: 'iconText' | 'logoIcon';
+  leftIcon?: { icon: ReactNode; onPress: () => void; disabled?: boolean };
+  rightIcon?: { icon: ReactNode; onPress: () => void; disabled?: boolean };
+};
+
+export function AppBar({
+  title,
+  titleStyle = 'iconText',
+  leftIcon,
+  rightIcon,
+}: AppBarProps) {
+  return (
+    <div className='py-3 px-8 flex items-center h-14 w-full'>
+      <div className='w-2/12 items-center flex justify-start'>
+        {leftIcon && <button className=''>{leftIcon.icon}</button>}
+      </div>
+
+      <div className='w-8/12 items-center flex'>
+        {titleStyle == 'iconText' ? (
+          <div className={'w-full flex items-center justify-center gap-1'}>
+            <Image src={'/FloweryIcon.svg'} alt='Icon' width={24} height={24} />
+            <div className='items-center'>{title}</div>
+          </div>
+        ) : (
+          <div className={'w-full flex items-center justify-center '}>
+            {titleStyle == 'logoIcon' && (
+              <Image
+                src={'/FloweryLogo.svg'}
+                alt='Icon'
+                width={165 / 1.5}
+                height={16 / 1.5}
+              />
+            )}
+          </div>
+        )}
+      </div>
+
+      <div className='w-2/12 items-center flex justify-end'>
+        {rightIcon && <button className='rightIcon'>{rightIcon.icon}</button>}
+      </div>
+    </div>
+  );
+}
